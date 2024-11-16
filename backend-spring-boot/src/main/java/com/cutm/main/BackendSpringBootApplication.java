887deb5d8bb933f28b5c3f0651923e168269bc98
Parsuram Maharana <123405783+MPARSURAM99@users.noreply.github.com>
2024-11-17 03:04:46 +0530

    Spring boot backend creation and spring security configuration

---
+++ backend-spring-boot/src/main/java/com/cutm/main/BackendSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.cutm.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringBootApplication.class, args);
+		System.out.println("Hello CUTM E-commerse developers");
+	}
+
+}
---
+++ backend-spring-boot/src/main/java/com/cutm/main/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.cutm.main.config;
+
+public class JwtConstant {
+	
+	public static final String SECRET_KEY="wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
+	public static final String JWT_HEADER="Authorization";
+
+}
